5a5c5f8cd7f8ff74304913f0264ac0db2a255a33
pravnkay <github@praveen.bulc.club>
2025-09-02 22:11:19 +0530

    Rewrite

--- Modules/Core/Core/Resources/views/components/datatable/select_filter.blade.php
+++
@@ -1,7 +0,0 @@
-<select class=\"uk-select uk-form-sm\">
-		<option value=""></option>
-	@foreach($options as $key => $value)
-		<option value=\"{{$value}}\">{{$value}}</option>
-	@endforeach
-</select>
-
--- Modules/Core/Core/Resources/views/components/errors.blade.php
+++
@@ -1,5 +0,0 @@
-@error($name)
-	<div class="uk-anmt-shake uk-form-help mt-2 text-destructive" id="{$name}-help-block">
-		{{ $message }}
-	</div>
-@enderror
